058608c7bc66be98d8c143c909165c148c16fadd
Jesse V. Burke <jburke@redskyalliance.org>
2016-09-27 08:01:05 -0400

    Delete git.sh

--- git.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-echo "# Stegosploit_Demos" >> README.md
-git init
-git add *
-git commit -m "first"
-git remote add origin https://github.com/JburkeRSAC/Stegosploit_Demos.git
-git push -u origin master
